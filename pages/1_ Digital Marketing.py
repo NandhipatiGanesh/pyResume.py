347abfd392f7448ec607bf43d9c515158f40d4b2
NandhipatiGanesh <nandhipatiganeshkumar16@gmail.com>
2023-03-25 01:04:30 +0530

    add

--- pages/1_ Digital Marketing.py
+++ pages/1_ Digital Marketing.py
@@ -1,28 +1,16 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="Digital Marketing Resume",
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+#hide unwanted text
+st.set_option('deprecation.showfileUploaderEncoding', False)
+    
+#opening the image
+image = Image.open ('./Images/New Passport profile photo.png')
 
-st.sidebar.success("Select a demo above.")
-
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+#displaying the image on streamlit app
+st.image (image, caption='')
